57bc11f16b959c7e7fd2f952d28c81f79dde8968
christinebyron <59991513+christinebyron@users.noreply.github.com>
2020-03-10 14:13:48 -0700

    to deploy app

--- app.py
+++ app.py
@@ -4,13 +4,12 @@
 Commandline tool for interacting with library
 """
 
+import pickle 
+from google.cloud import storage 
 import pandas as pd
 from flask import Flask, jsonify, request
-import pickle
+import sklearn
 
-# load model
-model = pickle.load(open('MSDS434_Final-Project-MVP/model.pkl','rb'))
- 
 # app
 app = Flask(__name__)
 
@@ -24,6 +23,16 @@ def predict():
     # convert data into dataframe
     data.update((x, [y]) for x, y in data.items())
     data_df = pd.DataFrame.from_dict(data)
+    
+    # set up access to the GCS bucket
+    bucket_name = "citibikenyc_app"
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    
+    # download and load the model
+    blob = bucket.blob("sMSDS434_Final-Project-MVP")
+    blob.download_to_filename("/tmp/lmodel.pkl")
+    model = pk.load(open("/tmp/model.pkl", 'rb'))
 
     # predictions
     result = model.predict(data_df)
